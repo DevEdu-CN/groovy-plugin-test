003166f62081825d0d9f745e92c771c50e9c74e6
DevEdu-CN <luiz.ed2013@gmail.com>
2023-10-22 20:17:17 -0300

    end

---
+++ src/main/groovy/com/plugintest/plugins/filediff/FileDiffExtension.groovy
@@ -0,0 +1,9 @@
+package com.plugintest.plugins.filediff
+
+
+import org.gradle.api.file.RegularFileProperty
+
+abstract class FileDiffExtension {
+    abstract RegularFileProperty getFile1()
+    abstract RegularFileProperty getFile2()
+}
